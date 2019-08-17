2453046e452a67a1f18fa666fd3f30c55dcf0b8a
chanyou0311 <chanyou0311@gmail.com>
2019-08-17 12:51:24 +0900

    [add] slack通知機能実装

--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,24 @@
-function notifySlack(message: string) {}
+interface payloadInterface {
+  text: string;
+}
+
+const slackWebhookUrl: string = PropertiesService.getScriptProperties().getProperty(
+  "slackWebhookUrl"
+);
+
+function notifySlack(message: string) {
+  const payloadData: payloadInterface = {
+    text: message
+  };
+
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(payloadData)
+  };
+  UrlFetchApp.fetch(slackWebhookUrl, options);
+}
+
+function main() {
+  notifySlack("hey!");
+}
